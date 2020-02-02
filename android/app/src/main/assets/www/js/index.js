fda2a53b3a4e8d7d835f65e5aedcaf05753cc001
Swetha Thoomoju <swetha.thoomoju@cariboutech.com>
2020-02-02 07:29:44 +0530

    Corrected the check in registerMessageListener

--- android/app/src/main/assets/www/js/index.js
+++ android/app/src/main/assets/www/js/index.js
@@ -44,7 +44,7 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        alert('Received Event: ' + id);
+        console.log('Received Event: ' + id);
 
         // Receiving messages from Watch
         var receiveMessageSuccess = function (message) {
